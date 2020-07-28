f597cee3aa9a3ad23ac1ea880d21445bad5c646c
benny-fung <68138908+benny-fung@users.noreply.github.com>
2020-07-28 15:28:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var getAllRecords = function() {
     $.getJSON(
-      `https://api.airtable.com/v0/appjwpiqXF4HFv6V0/Page?api_key=${secrets.KEY}`,
+      `https://api.airtable.com/v0/appjwpiqXF4HFv6V0/Page?api_key=${KEY}`,
       function(airtable) {
         var html = [];
         $.each(airtable.records, function(index, record) {
